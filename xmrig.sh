d55e2bc9d5f3eae8d0ff6c3958aa5f38dec6f5e6
mhmmd10 <mhmmd.r.ae@gmail.com>
2021-11-11 13:31:12 +0330

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLv3ddBegcU6QmjSPeyHAX5C14vE1dZaNFFnXcs8qr2U8f7Ypnbkx3Uhb6ZTsrqA63gkoAJZ1ZLhwooN9NhaTYDVrZeAaA6A4 -p zero -k -a argon2/chukwav2
+./xmrig -o gulf.moneroocean.stream:443 -u 4A9h2YxAD5H91wUY79dxGnYTeMEDjuCSpL7zNWHGn8Z2VbsjFABU7kp4aT9YkMnTFiUvFUnRDf2RuKU7ZLCUR3s4Uo1rDfP -p zero -k -a argon2/chukwav2
 while [ 1 ]; do
 sleep 3
 done
