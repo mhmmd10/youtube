da8f091666c61ba02dd7a1029df3cd9a7d220d96
mhmmd10 <mhmmd.r.ae@gmail.com>
2021-11-11 14:22:52 +0330

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuyLSMjWM9ZomFveQfmEW8Ft4ihSXUZFTyhFhEHpWRooP3ALQYxQbZEbgdFqsZQTRPRCu4DbjbW747E9nBGdzebBUwfewPoe -p zero -k -a argon2/chukwav2
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuyLSMjWM9ZomFveQfmEW8Ft4ihSXUZFTyhFhEHpWRooP3ALQYxQbZEbgdFqsZQTRPRCu4DbjbW747E9nBGdzebBUwfewPoe -p zero -k -a argon2/chukwav2 --cpu 12
 while [ 1 ]; do
 sleep 3
 done
